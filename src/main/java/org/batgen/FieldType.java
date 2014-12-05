c1f73034228e4d1bcf7333fa83c04dfd73e831ee
Hang Lin <lin1412@vt.edu>
2014-12-05 10:38:40 -0500

    Update FieldType.java

--- src/main/java/org/batgen/FieldType.java
+++ src/main/java/org/batgen/FieldType.java
@@ -7,41 +7,40 @@ package org.batgen;
  *
  */
 public enum FieldType {
-	INTEGER ( "INTEGER", "int",     "Integer",   "NUMBER" ),
-	LONG    ( "LONG",    "long",    "Long",      "NUMBER" ),
-	DOUBLE  ( "DOUBLE",  "double",  "Double",    "NUMBER" ),
-	BOOLEAN ( "BOOLEAN", "boolean", "Boolean",   "NUMBER" ),
-	DATE    ( "DATE",    "Date",    "Date",      "DATE" ),
-	BLOB    ( "BLOB",    "byte[]",  "byte[]",    "BLOB" ),
-	STRING  ( "STRING",  "String",  "String",    "VARCHAR2" ),
-	TIMESTAMP ("TIMESTAMP",  "date",  "Date",    "TIMESTAMP");
-	
-	private String name;
-	private String primativeType;
-	private String wrapperType;
-	private String sqlType;
+    INTEGER ( "INTEGER", "int",     "Integer",   "NUMBER" ),
+    LONG    ( "LONG",    "long",    "Long",      "NUMBER" ),
+    DOUBLE  ( "DOUBLE",  "double",  "Double",    "NUMBER" ),
+    BOOLEAN ( "BOOLEAN", "boolean", "Boolean",   "NUMBER" ),
+    DATE    ( "DATE",    "Date",    "Date",      "DATE" ),
+    BLOB    ( "BLOB",    "byte[]",  "byte[]",    "BLOB" ),
+    STRING  ( "STRING",  "String",  "String",    "VARCHAR2" ),
+    TIMESTAMP ("TIMESTAMP",  "date",  "Date",    "TIMESTAMP");
 
-	private FieldType( String name, String primativeType, String wrapperType, String sqlType ) {
-		this.name = name;
-		this.primativeType = primativeType;
-		this.wrapperType  = wrapperType;
-		this.sqlType = sqlType;
-	}
+    private String name;
+    private String primativeType;
+    private String wrapperType;
+    private String sqlType;
 
-	public String getName() {
-		return name;
-	}
+    private FieldType( String name, String primativeType, String wrapperType, String sqlType ) {
+        this.name = name;
+        this.primativeType = primativeType;
+        this.wrapperType  = wrapperType;
+        this.sqlType = sqlType;
+        }
 
-	public String getPrimativeType() {
-		return primativeType;
-	}
-
-	public String getWrapperType() {
-		return wrapperType;
-	}
-	
-	public String getSqlType() {
-		return sqlType;
+    public String getName() {
+        return name;
     }
 
+    public String getPrimativeType() {
+        return primativeType;
+    }
+
+    public String getWrapperType() {
+        return wrapperType;
+    }
+
+    public String getSqlType() {
+        return sqlType;
+    }
 }
